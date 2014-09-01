46fb1e38065784f292f64604bacc0fe16ed8b5fa
shrek <jermyn.shi@foxmail.com>
2014-09-01 15:46:51 +0800

    updte public/hd

--- public/hd/js/main.js
+++ public/hd/js/main.js
@@ -1,115 +1,102 @@
 /*------------------------------------------------------------------------
  * Copyright (c) 2014 hailiang All rights reserved. by Jackal
- ------------------------------------------------------------------------*/
+------------------------------------------------------------------------*/
 
-var _namespace_microLetter = {}
+$(function(){
+	
+	$("body").append('<div class="mask"><div class="dailog"><h2>人品大挑战</h2><span></span><a href="###"></a></div></div>');
 
-
-$(function () {
-
-
-    $(".indexbg").click(function () {
-        $(this).addClass("dismiss");
-        //$(this).removeClass("show");
-        document.querySelector(".indexbg").addEventListener("webkitAnimationEnd", function () {
-            $(this).removeClass("show");
-            $(".page").addClass("show");
-        })
-    });
-
-    $(".mask").click(function () {
-        $(this).removeClass("show");
-    });
-
-    $(".finish").click(function () {
-        checkSubmitAll();
-    });
-
-    $(".share").click(function () {
-        location.href = "support.html";
-    });
+	$(".indexbg").click(function(){ 
+		$(this).addClass("dismiss");
+		//$(this).removeClass("show");
+		document.querySelector(".indexbg").addEventListener("webkitAnimationEnd", function(){
+			$(this).removeClass("show");
+			$(".page").addClass("show");
+		})
+	});
+	
+	$(".mask").click(function(){ 
+		$(this).removeClass("show");
+	});
+	
+	$(".finish").click(function(){ 
+		checkSubmitAll();
+	});
+	
+	$(".share").click(function(){ 
+		location.href = "support.html";
+	});
 
 
-    $(".agree").click(function () {
-        location.href = "agree.html";
-    });
-    $(".disagree").click(function () {
-        location.href = "disagree.html";
-    });
+	$(".agree").click(function(){ 
+		location.href = "agree.html";
+	});
+	$(".disagree").click(function(){ 
+		location.href = "disagree.html";
+	});
+	
+	$(".loginbtn").click(function(){ 
+		location.href = "status.html";
+		
+	});
+	
+	$(".retry").click(function(){ 
+		location.href = "index.html";
+	});
+	
+	$(".goodguy").click(function(){ 
+		location.href = "goodguy.html";
+	});
+	
+	$(".badguy").click(function(){ 
+		location.href = "badguy.html";
+	});
+	
+	$(".sharetipmask").click(function(){ 
+		$(this).removeClass("show");
+	});
 
-    $(".loginbtn").click(function () {
-        location.href = "status.html";
-
-    });
-
-    $(".retry").click(function () {
-        location.href = "index.html";
-    });
-
-    $(".goodguy").click(function () {
-        location.href = "goodguy.html";
-    });
-
-    $(".badguy").click(function () {
-        location.href = "badguy.html";
-    });
-
-    $(".sharetipmask").click(function () {
-        $(this).removeClass("show");
-    });
-
-    $(".sending").click(function () {
-        alert("支持成功！立即开始我的人品测试");
-        location.href = "index.html";
-    });
-
-    _namespace_microLetter.loadMask();
-
-    $(".mask").click(function () {
-        $(this).removeClass("show");
-    });
-
+	$(".sending").click(function(){ 
+		alert("支持成功！立即开始我的人品测试");
+		location.href = "index.html";
+	});
+	
+	
 });
 
-// 加载弹出层
-_namespace_microLetter.loadMask = function () {
-    $("body").append('<div class="mask"><div class="dailog"><h2>人品大挑战</h2><span></span><a href="###"></a></div></div>');
-}
-
-
-function alertView(wrongmessage) {
-    $(".mask").addClass("show");
-    $(".dailog span").text(wrongmessage);
+function alertView(wrongmessage){ 
+		$(".mask").addClass("show");
+		$(".dailog span").text(wrongmessage);
 }
 
 
 //验证手机
-function checkSubmitAll() {
-    if ($(".say").val() == "") {
-        alert("内容不能为空！")
-        $(".say").focus();
-        return false;
-    }
+function checkSubmitAll(){ 
+	if($(".say").val()==""){ 
+		alert("内容不能为空！")
+		$(".say").focus();
+		return false; 
+	} 
+	
+	if($(".address").val()==""){ 
+		alert("地址不能为空！")
+		$(".address").focus();
+		return false; 
+	} 
 
-    if ($(".address").val() == "") {
-        alert("地址不能为空！")
-        $(".address").focus();
-        return false;
-    }
+	if($(".phone").val()==""){ 
+		alert("手机号码不能为空！")
+		$(".phone").focus();
+		return false; 
+	} 
 
-    if ($(".phone").val() == "") {
-        alert("手机号码不能为空！")
-        $(".phone").focus();
-        return false;
-    }
-
-    if (!$(".phone").val().match(/^1[3|4|5|8][0-9]\d{4,8}$/)) {
-        alert("手机号码格式不正确！请重新输入！")
-        $(".phone").focus();
-        return false;
-    }
-
-    location.href = "confirm.html";
+	if(!$(".phone").val().match(/^1[3|4|5|8][0-9]\d{4,8}$/)){ 
+		alert("手机号码格式不正确！请重新输入！")
+		$(".phone").focus();
+		return false; 
+   } 
+	
+	location.href = "confirm.html";
 
 }
 
